4bb4e1f70beebd4da5e79d7ea5b61f2c1be13d91
1e3qr <1e3qr.kos@gmail.com>
2026-04-05 19:50:03 +0200

    replaced deprecatet feature

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,9 +5,19 @@ return {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
 		"nvim-tree/nvim-web-devicons",
-		"folke/snacks.nvim",
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		window = {
+			mappings = {
+				["P"] = {
+					"toggle_preview",
+					config = {
+						use_float = true,
+						use_snacks_image = true,
+					},
+				},
+			},
+		}
 	end,
 }
