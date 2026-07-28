22f3d5464ed6d8bb2aaf16f763822e1c96298cf8
1e3qr <1e3qr.kos@gmail.com>
2026-07-28 13:56:21 +0200

    changed scroll keys for snippets to tab and s-tab for down

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,12 +13,13 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -30,7 +31,41 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+					-- Enter only confirms if an item has been explicitly selected.
+					-- Otherwise it just inserts a normal newline.
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() and cmp.get_selected_entry() then
+							cmp.confirm({ select = false })
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					-- Tab only moves the highlight to the next item, it does NOT
+					-- confirm/insert it. If nothing is completing, it jumps to
+					-- the next snippet placeholder instead.
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					-- Shift-Tab: move highlight to previous item (no confirm),
+					-- or jump to previous snippet placeholder.
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
